df3a291e2a798955e1bccab0a2e304411e744445
Damien <damien@damienvalade.fr>
2020-02-11 22:15:28 +0100

    Verify compatibility
    Verify compatibility

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,17 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
      * @param Request $request
+     * @param AuthenticationUtils $authenticationUtils
      * @return Response
      */
-    public function loginAction(Request $request)
+    public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
     {
-        $authenticationUtils = $this->get('security.authentication_utils');
 
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
