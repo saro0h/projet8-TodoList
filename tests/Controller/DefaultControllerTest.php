604b8f393819686eab31943fbfc82f29cf14363e
abdounikarim <abdounikarim@gmail.com>
2020-04-07 18:37:41 +0200

    add some tests

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -2,11 +2,61 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultControllerTest extends WebTestCase
 {
+    private function getService($serviceName)
+    {
+        self::bootKernel();
+        $service = self::$container
+            ->get($serviceName);
+        self::ensureKernelShutdown();
+        return $service;
+    }
+
+    private function getTasks()
+    {
+        return $this
+            ->getService('doctrine.orm.default_entity_manager')
+            ->getRepository(Task::class)
+            ->findAll()
+        ;
+    }
+
+    private function login($username, $password)
+    {
+        return static::createClient([], [
+            'PHP_AUTH_USER' => $username,
+            'PHP_AUTH_PW'   => $password,
+        ]);
+    }
+
+    public function testTasksPage()
+    {
+        $client = $this->login('toto', 'toto');
+        $client->request('GET', '/tasks');
+
+        $this->assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+    }
+
+    public function testTaskPage()
+    {
+        $tasks = $this->getTasks();
+        /**
+         * @var Task $task
+         */
+        foreach ($tasks as $task) {
+            $client = $this->login('toto', 'toto');
+            $client->request('GET', '/tasks/'.$task->getId().'/edit');
+
+            $this->assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+            self::ensureKernelShutdown();
+        }
+    }
+
     public function testHomepage()
     {
         $client = static::createClient();
@@ -17,10 +67,7 @@ class DefaultControllerTest extends WebTestCase
 
     public function testHomepageWithLoggedUser()
     {
-        $client = static::createClient([], [
-            'PHP_AUTH_USER' => 'toto',
-            'PHP_AUTH_PW'   => 'pass',
-        ]);
+        $client = $this->login('toto', 'toto');
         $client->request('GET', '/');
 
         $this->assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
