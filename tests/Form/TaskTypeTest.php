40224f837042b0d3b6735cbd3ed526d458233241
Dogukan Cirpan <cirpan.dogukan5959@gmail.com>
2021-02-10 10:17:11 +0100

    Unit Tests (IsNotDonne, ByUserAnonyme, ByUserNotAnonyme)

--- tests/Form/TaskTypeTest.php
+++ tests/Form/TaskTypeTest.php
@@ -2,6 +2,7 @@
 namespace App\Tests\Form;
 
 use App\Entity\Task;
+use App\Entity\User;
 use App\Form\TaskType;
 use Symfony\Component\Form\Test\TypeTestCase;
 
@@ -10,8 +11,8 @@ class TaskTypeTest extends TypeTestCase
     public function testCreateTaskSuccess()
     {
         $date = new \DateTime;
-        $date->setDate('2020', '07', '01');
-        $date->setTime('19', '00', '00');
+        $date->setDate('2021', '01', '20');
+        $date->setTime('10', '00', '00');
 
         $formData = [
             'title' => 'testtask',
@@ -21,6 +22,7 @@ class TaskTypeTest extends TypeTestCase
 
         $model = new Task();
         $model->setCreatedAt($date);
+
         $form = $this->factory->create(TaskType::class, $model);
 
         $expected = new Task();
@@ -35,11 +37,42 @@ class TaskTypeTest extends TypeTestCase
         $this->assertEquals($expected, $model);
     }
 
+    public function testCreateTaskIsNotDoneSuccess()
+    {
+        $date = new \DateTime;
+        $date->setDate('2021', '01', '20');
+        $date->setTime('10', '00', '00');
+
+        $formData = [
+            'title' => 'testtask',
+            'content' => 'testtask2',
+            'createdAt' => $date
+        ];
+
+        $model = new Task();
+        $model->setCreatedAt($date);
+        $model->setIsDone(0);
+
+        $form = $this->factory->create(TaskType::class, $model);
+
+        $expected = new Task();
+        $expected->setTitle('testtask');
+        $expected->setContent('testtask2');
+        $expected->setCreatedAt($date);
+        $expected->setIsDone(0);
+
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+
+        $this->assertEquals($expected, $model);
+    }
+
     public function testCreateTaskIsDoneSuccess()
     {
         $date = new \DateTime;
-        $date->setDate('2020', '07', '01');
-        $date->setTime('19', '00', '00');
+        $date->setDate('2021', '01', '20');
+        $date->setTime('10', '00', '00');
 
         $formData = [
             'title' => 'testtask',
@@ -66,7 +99,65 @@ class TaskTypeTest extends TypeTestCase
         $this->assertEquals($expected, $model);
     }
 
-    public function testDeleteTaskSuccess()
-    {}
+    public function testCreateTaskByUserAnonymeSuccess()
+    {
+        $date = new \DateTime;
+        $date->setDate('2021', '01', '20');
+        $date->setTime('10', '00', '00');
 
+        $formData = [
+            'title' => 'testtask',
+            'content' => 'testtask2',
+            'createdAt' => $date
+        ];
+
+        $model = new Task();
+        $model->setCreatedAt($date);
+        $model->setUser(NULL);
+
+        $form = $this->factory->create(TaskType::class, $model);
+
+        $expected = new Task();
+        $expected->setTitle('testtask');
+        $expected->setContent('testtask2');
+        $expected->setCreatedAt($date);
+        $expected->setUser(NULL);
+
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+
+        $this->assertEquals($expected, $model);
+    }
+
+    public function testCreateTaskByUserNotAnonymeSuccess()
+    {
+        $date = new \DateTime;
+        $date->setDate('2021', '01', '20');
+        $date->setTime('10', '00', '00');
+
+        $formData = [
+            'title' => 'testtask',
+            'content' => 'testtask2',
+            'createdAt' => $date
+        ];
+
+        $model = new Task();
+        $model->setCreatedAt($date);
+        $model->setUser(new User());
+
+        $form = $this->factory->create(TaskType::class, $model);
+
+        $expected = new Task();
+        $expected->setTitle('testtask');
+        $expected->setContent('testtask2');
+        $expected->setCreatedAt($date);
+        $expected->setUser(new User());
+
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+
+        $this->assertEquals($expected, $model);
+    }
 }
