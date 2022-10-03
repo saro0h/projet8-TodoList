720799c264905981865f87696d8696a919a7705d
knouz15 <benknouz@yahoo.fr>
2022-10-03 15:42:16 +0200

    Rajout TaskVoter

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use App\Form\TaskType;
+use App\Security\Voter\TaskVoter;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -49,7 +50,7 @@ class TaskController extends AbstractController
      /**
      * @Route("/tasks/{id}/edit", name="task_edit")
      */
-    public function editAction(Task $task, Request $request)
+    public function editAction(Task $task, Request $request):Response
     {
         $form = $this->createForm(TaskType::class, $task);
 
@@ -86,15 +87,19 @@ class TaskController extends AbstractController
      */
     public function deleteTaskAction(Task $task):Response
     {
-        if ($task->getUser() === $this->getUser())
-        {
+        // if ($task->getUser() === $this->getUser())
+        // {
+        $this->denyAccessUnlessGranted(
+        // TaskVoter::DELETE
+        'DELETE'
+        , $task, 'Denied! Vous ne pouvez pas supprimer cette tâche');
         $em = $this->getDoctrine()->getManager();
         $em->remove($task);
         $em->flush();
 
         $this->addFlash('success', 'La tâche a bien été supprimée.');
-        }
-        $this->addFlash('error', 'Vous ne pouvez pas supprimer cette tâche.');
+        // }
+        // $this->addFlash('error', 'Vous ne pouvez pas supprimer cette tâche.');
         return $this->redirectToRoute('task_list');
     }
 }
