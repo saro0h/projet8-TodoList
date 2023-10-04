272a51769f06b38e3323fe1378771839ff05a5fa
axelle <kadance9720@gmail.com>
2023-10-04 12:24:45 +0200

    Update to 3.4 #1 and fix symfony deprecations #2

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultControllerTest extends WebTestCase
 {
